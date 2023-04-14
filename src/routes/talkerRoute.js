2148a25d3fa055bd1d50027b6c7d199acba0d5e1
Miguel Raatz <miguelraatz_@hotmail.com>
2023-04-14 15:11:03 -0300

    feat: logical requirement implemented 1

---
+++ src/routes/talkerRoute.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const fs = require('fs');
+const { getAllManagers } = require('../utils/readAndWriteFiles.js')
+
+const talkerRoute = express.Router();
+
+talkerRoute.get('/', async(_req, res) => {
+  const managers = await getAllManagers();
+  if (!managers) return res.status(200).json([]);
+  return res.status(200).json(managers);
+});
+
+
+module.exports = talkerRoute;
---
+++ src/utils/readAndWriteFiles.js.js
@@ -0,0 +1,22 @@
+const fs = require('fs').promises;
+const { join } = require('path');
+
+const readTalkerManagerFile = async () => {
+  const path = '../talker.json';
+  try {
+    const contentFile = await fs.readFile(join(__dirname, path), 'utf-8');
+    return JSON.parse(contentFile);
+  } catch (err) {
+    return null;
+  }
+};
+
+const getAllManagers = async () => {
+  const allManagers = await readTalkerManagerFile();
+  return allManagers;
+}
+
+module.exports = {
+  readTalkerManagerFile,
+  getAllManagers,
+};
